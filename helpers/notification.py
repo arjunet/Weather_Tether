0029c89acca4bfe8955fd5fdcfd6abda00b866cf
Arjune Mithu <arjunemithu@gmail.com>
2026-06-15 12:22:32 -0400

    modded notifications to have a timer on them

--- helpers/notification.py
+++ helpers/notification.py
@@ -1,15 +1,21 @@
 from carbonkivy.uix.notification import CNotificationToast
+from kivy.clock import Clock
 
 def notification_error(subtitle):
-    return CNotificationToast(
+    notif = CNotificationToast(
         title="Error",
         status="Error",
         subtitle=subtitle
     )
 
+    Clock.schedule_once(lambda dt: notif.dismiss(), 4.5)
+    return notif 
+
 def notification_success(subtitle):
-    return CNotificationToast(
+    notif = CNotificationToast(
         title="Success",
         status="Success",
         subtitle=subtitle
     )
+    Clock.schedule_once(lambda dt: notif.dismiss(), 4.5)
+    return notif
